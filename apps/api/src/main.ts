99221966ed37dc828a098f7bc6d73dc2c7e201f6
caiohenrqq <caiohenrqq@gmail.com>
2026-03-14 23:13:56 -0400

    build(api): switch nest runtime from express to fastify

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,8 @@
-import { AppModule } from '@app/app.module';
+import { createHttpApp } from '@app/common/http/http-app.factory';
 import { AppSettingsService } from '@app/common/settings/app-settings.service';
-import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await createHttpApp();
 	const appSettings = app.get(AppSettingsService);
 
 	await app.listen(appSettings.port, '0.0.0.0');
